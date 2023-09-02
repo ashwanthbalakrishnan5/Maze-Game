a43e862e0520ee7e1608c6500f5f6d30c9aefbc6
Ashwanth <ashwanthbalakrishnan5@gmail.com>
2023-09-02 10:32:26 +0530

    made the game playable

--- public/script.js
+++ public/script.js
@@ -66,42 +66,83 @@ const mazeEnd = [mazeWidth - 1, mazeHeight - 1];
 
 const maze = generateMaze(mazeWidth, mazeHeight, mazeStart, mazeEnd);
 
-const mazeContainer = document.createElement("div");
+const mazeContainer = document.getElementById('mazeContainer');
 mazeContainer.style.display = "grid";
 mazeContainer.style.gridTemplateColumns = `repeat(${mazeWidth * 2 + 1}, 20px)`;
+
+const containerWidth = (mazeWidth * 2 + 1) * 20;
+mazeContainer.style.width = `${containerWidth}px`;
+
+mazeContainer.style.position = "absolute";
+mazeContainer.style.left = "50%";
+mazeContainer.style.transform = "translateX(-50%)";
 
 for (let y = 0; y < maze.length; y++) {
   for (let x = 0; x < maze[y].length; x++) {
     const cell = document.createElement("div");
     cell.className = "cell";
     cell.style.backgroundColor = maze[y][x] ? "black" : "white";
+    if (x === 0 && y === 1) {
+      cell.style.backgroundColor = "green";
+    }
+    else if (x === mazeWidth * 2 && y === mazeHeight * 2 - 1) {
+      cell.style.backgroundColor = "white";
+    }
     mazeContainer.appendChild(cell);
   }
 }
 
 document.body.appendChild(mazeContainer);
 
-// document.getElementById("generateButton").addEventListener("click", function () {
-//   const mazeSizeInput = document.getElementById("mazeSize");
-//   const mazeSize = parseInt(mazeSizeInput.value);
+let playerPosition = [0, 1]
 
-//   if (!isNaN(mazeSize)) {
-//     const mazeStart = [0, 0];
-//     const mazeEnd = [mazeSize - 1, mazeSize - 1];
+function isValidMove(x, y) {
+  if (x < 0 || x >= mazeWidth * 2 + 1 || y < 0 || y >= mazeHeight * 2 + 1) {
+    return false;
+  }
+  else if (x === mazeWidth * 2 && y === mazeHeight * 2 - 1) {
+    return true;
+  }
+  return maze[y][x] === false;
+}
 
-//     const maze = generateMaze(mazeSize, mazeSize, mazeStart, mazeEnd);
-//     const mazeContainer = document.getElementById("mazeContainer");
-//     mazeContainer.innerHTML = "";
-//     mazeContainer.style.display = "grid";
-//     mazeContainer.style.gridTemplateColumns = `repeat(${mazeSize * 2 + 1}, 20px)`;
+function movePlayer(dx, dy) {
+  const newX = playerPosition[0] + dx;
+  const newY = playerPosition[1] + dy;
+  if (isValidMove(newX, newY)) {
 
-//     for (let y = 0; y < maze.length; y++) {
-//       for (let x = 0; x < maze[y].length; x++) {
-//         const cell = document.createElement("div");
-//         cell.className = "cell";
-//         cell.style.backgroundColor = maze[y][x] ? "black" : "white";
-//         mazeContainer.appendChild(cell);
-//       }
-//     }
-//   }
-// });
+    previousIndex = playerPosition[1] * (mazeWidth * 2 + 1) + playerPosition[0];
+    currentIndex = newY * (mazeWidth * 2 + 1) + newX;
+
+    const previousCell = mazeContainer.children[previousIndex]; 
+    const currentCell = mazeContainer.children[currentIndex];
+
+    previousCell.style.transition = "background-color 0.3s ease-in-out";
+    currentCell.style.transition = "background-color 0.3s ease-in-out";
+
+    previousCell.style.backgroundColor = maze[playerPosition[1]][playerPosition[0]] ? "black" : "white";
+    currentCell.style.backgroundColor = "green";
+
+    playerPosition = [newX, newY];
+
+    if (playerPosition[0] === mazeWidth * 2 && playerPosition[1] === mazeHeight * 2 - 1) {
+      alert("Congratulations! You reached the end of the maze!");
+    }
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+    var code = e.which || e.keyCode;
+    if (code == '38') {
+      movePlayer(0, -1);
+    }
+    else if (code == '40') {
+      movePlayer(0, 1);
+    }
+    else if (code == '37') {
+      movePlayer(-1, 0);
+    }
+    else if (code == '39') {
+      movePlayer(1, 0);
+    }
+})
